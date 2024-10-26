ea53e96ec8a9aab56e36dbcd84be0e4d9865fe41
Naman Sharma <namanks0511@gmail.com>
2024-10-26 22:35:55 +0530

    splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/core/configs/theme/app_theme.dart';
+import 'package:spotify_clone/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,11 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-        ),
-      ),
-    );
+      home: const Splash(),
+      );
   }
 }
